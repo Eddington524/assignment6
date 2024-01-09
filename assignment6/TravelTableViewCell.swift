7d60e3fa602fc6f96b9250f70d3cadecdf28fffb
eddington524 <iamsam8687@gmail.com>
2024-01-09 10:47:47 +0900

    feat: magazine assignmet

--- assignment6/TravelTableViewCell.swift
+++ assignment6/TravelTableViewCell.swift
@@ -9,15 +9,9 @@ import UIKit
 
 class TravelTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+ 
+    @IBOutlet var thumnailImage: UIImageView!
+    @IBOutlet var mainTitle: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
 }
--- assignment6/TravelTableViewController.swift
+++ assignment6/TravelTableViewController.swift
@@ -6,21 +6,63 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelTableViewController: UITableViewController {
 
+    @IBOutlet var HeaderView: UIView!
+    @IBOutlet var headerTitle: UILabel!
+    
+    let list: [Magazine] = MagazineInfo().magazine
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerTitle.text = "SeSAC TRAVEL"
+        
+        tableView.rowHeight = 500
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
+    //1. 셀 갯수
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return list.count
     }
-
-   
+    
+    //2. 셀 디자인
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "travelViewCell", for: indexPath) as! TravelTableViewCell
+        
+        let mainTitle = list[indexPath.row].title
+        let link = list[indexPath.row].photo_image
+        let subTitle = list[indexPath.row].subtitle
+        let dateText = list[indexPath.row].date
+        
+        let url = URL(string: link)
+        
+        cell.thumnailImage.kf.setImage(with: url)
+        cell.thumnailImage.layer.cornerRadius = 10
+        
+        cell.mainTitle.text = mainTitle
+        cell.mainTitle.font = .systemFont(ofSize: 20)
+        
+        cell.subTitleLabel.text = subTitle
+        cell.subTitleLabel.textColor = .gray
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        
+        if let date = dateFormatter.date(from: list[indexPath.row].date){
+            
+            let newDateFomatter = DateFormatter()
+            newDateFomatter.dateFormat = "yy년 MM월 dd일"
+            let formattedDate = newDateFomatter.string(from: date)
+            
+            cell.dateLabel.text = formattedDate
+        }else{
+            print("날짜 변환 실패")
+        }
+        
+        return cell
+    }
 
 }
