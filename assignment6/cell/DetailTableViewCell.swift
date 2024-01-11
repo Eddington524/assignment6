4cb04a25b65aa1985cc29579d3ceb97c5b35196f
eddington524 <iamsam8687@gmail.com>
2024-01-12 02:28:13 +0900

    feat: 광고, 관광지 table셀 포함 테이블 뷰구현

---
+++ assignment6/cell/DetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailTableViewCell.swift
+//  assignment6
+//
+//  Created by Sammy Jung on 2024/01/11.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
