347eff09d4a00e5325739d60ced4ff378030c091
eddington524 <iamsam8687@gmail.com>
2024-01-11 17:50:45 +0900

    add: city detail 컨트롤러, 셀 추가

---
+++ assignment6/cell/AdTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AdTableViewCell.swift
+//  assignment6
+//
+//  Created by Sammy Jung on 2024/01/11.
+//
+
+import UIKit
+
+class AdTableViewCell: UITableViewCell {
+    
+static let identifier = "AdTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ assignment6/citydetail/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  assignment6
+//
+//  Created by Sammy Jung on 2024/01/11.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
